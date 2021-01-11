24ca2978e689b527980243c8ba5a7c88214f442c
DawnDesign7 <dawnmoraa@gmail.com>
2021-01-11 03:03:04 +0300

    add a confirm dialog to ask client if they'd prefer delivery service, and prompt to collect delivery location.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// to compile orger into a sentence
+document.getElementById("sentence").innerHTML = 5 + 6;
+
+// Delivery alert and collect location
+function deliveryAlert() {
+  var delivery = confirm ("Would you like Home Delivery? It's 100/= only!!!");
+if (delivery = true) {
+  var location = prompt ("Enter your location here");
+}
+}

1cb83e370a32740531d1d7cd67ed92582ba3cdd7
DawnDesign7 <dawnmoraa@gmail.com>
2021-01-11 07:45:30 +0300

    edit checkOut function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
-// to compile orger into a sentence
-document.getElementById("sentence").innerHTML = 5 + 6;
+// to compile order into a sentence
+//document.getElementById("sentence").innerHTML = 5 + 6;
 
 // Delivery alert and collect location
 function deliveryAlert() {
   var delivery = confirm ("Would you like Home Delivery? It's 100/= only!!!");
-if (delivery = true) {
+if (delivery === true) {
   var location = prompt ("Enter your location here");
+ }
 }
-}
+// if (location = "")
+//alert("Enter Location!!!")
+// }
+
+//Radio Inputs
+ var size = document.getElementById('size');
+ var crust = document.getElementById('crust');
+ var topping = document.getElementById('topping')
+ console.log(size);
+
+
+
+ // Check-out button
+ function checkOut() {
+   alert("Your total cost is --- inclusive of delivery charges")
+ }
